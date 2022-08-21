3bce3e9a4cfec23df3580894ad80b9ab5e6399f8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-08-21 11:31:59 +0200

    CPH1717: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: oppo Model: CPH1717 Script version: 2.4.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_CPH1717.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_CPH1717-user \
+    omni_CPH1717-userdebug \
+    omni_CPH1717-eng
